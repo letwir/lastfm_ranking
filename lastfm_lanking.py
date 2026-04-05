1bcc46224972e0c372ba65d912ab51da7f81b11f
letwir <letwirwinkuru@gmail.com>
2026-04-06 05:17:06 +0900

    change: TEMPLATE

--- lastfm_lanking.py
+++ lastfm_lanking.py
@@ -22,11 +22,11 @@ KIKAN: str = ""  # ランキングの期間を表す文字列（例: "今週", "
 
 # ---- ログの設定 ----
 logging.basicConfig(
-    level=logging.WARN,  # ログレベルをWARNINGに設定（必要に応じてDEBUGなどに変更可能）
+    level=logging.INFO,  # ログレベルをWARNINGに設定（必要に応じてDEBUGなどに変更可能）
     format="%(asctime)s - %(levelname)s - %(message)s",  # ログのフォーマット
     handlers=[
         logging.FileHandler("lastfm_ranking.log"),  # ログをファイルに出力
-        logging.StreamHandler(sys.stdout),  # ログをコンソールにも出力
+        # logging.StreamHandler(sys.stdout),  # ログをコンソールにも出力
     ],
 )
 
@@ -54,25 +54,27 @@ def make_text(data: str, KIKAN: str, max_char_len: int) -> str:
     result = data.get("toptracks", {}).get("track", [])
     for i in range(len(result)):
         track = result[i]
-        name = track.get("name", "")  # 曲名をmax_char_len文字に切り詰める
+        name = track.get("name", "")[
+            :max_char_len
+        ]  # 曲名をmax_char_len文字に切り詰める
         artist = track.get("artist", {}).get("name", "")[
             :max_char_len
         ]  # アーティスト名をmax_char_len文字に切り詰める
-        playcount = track.get("playcount", "0")[
-            :max_char_len
-        ]  # 再生回数を取得（存在しない場合は0を使用）
-        pre_ranking = f"{i + 1}. {name} - {artist} {playcount}回\n"
+        playcount = track.get(
+            "playcount", "-"
+        )  # 再生回数を取得（存在しない場合は0を使用）
+        pre_ranking = f"{i + 1}. [{playcount}回] {name} \n\t🎤{artist}\n"
         pre_text += unicodedata.normalize(
             "NFKC", pre_ranking
         )  # Unicode正規化pre_ranking
-    logging.debug(f"text length: {len(pre_text)} characters")
     return pre_text[:-1]  # 最後の改行を削除して返す
 
 
-def text_length_fix(text: str, data: dict, kikan: str, char_len: int) -> str:
+def text_length_fix(data: dict, kikan: str, char_len: int) -> str:
     """
     ツイートの文字数を500文字程度に調整
     """
+    text = make_text(data, kikan, max_char_len=char_len)
     while len(text) < 500:
         char_len += 1
         text = make_text(data, kikan, max_char_len=char_len)
@@ -89,13 +91,11 @@ def text_length_fix(text: str, data: dict, kikan: str, char_len: int) -> str:
 
 
 def main():
-    char_len = 30  # 曲名とアーティスト名の最大文字数を初期値として設定
+    char_len = 48  # 曲名とアーティスト名の最大文字数を初期値として設定
     data = fetch_lastfm_json()
     logging.debug(f"Fetched data: {data}")
-    text: str = make_text(data, KIKAN, max_char_len=char_len)
-    logging.debug(f"Initial text length: {len(text)} characters")
-    text = text_length_fix(text, data, KIKAN, char_len)
-    logging.debug(f"Final text length: {len(text)} characters")
+    text = text_length_fix(data, KIKAN, char_len)
+    logging.info(f"Final text length: {len(text)} characters")
     print(text)
 
 
